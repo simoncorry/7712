d95b41fb38c53ea71ef941e691b4dc1f3ccf7e22
simoncorry <simoncorry@gmail.com>
2012-06-11 10:57:19 -0400

    Setup

---
+++ _includes/fb-og.php
@@ -0,0 +1,9 @@
+        
+        <!-- /// FACEBOOK OG /// -->
+        <meta name="" property="og:title" content="77 12th - Luxury High-Rise Apartments - Midtown Atlanta"/>
+        <meta name="" property="og:type" content="company"/>
+        <meta name="" property="og:image" content="/_media/images/id/facebook.jpg"/>
+        <meta name="" property="og:url" content="http://www.7712th.com/"/>
+        <meta name="" property="og:site_name" content="77 12th - Luxury High-Rise Apartments - Midtown Atlanta"/>
+        <meta name="" property="og:description" content="77 12th - Luxury High-Rise Apartments - Midtown Atlanta"/>
+        <meta name="" property="fb:admins" content="512859885"/>

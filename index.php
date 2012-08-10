0d1c9d001c0e0fd44e591da30a9a5f736e85130d
simoncorry <simoncorry@gmail.com>
2012-08-10 13:25:11 -0400

    flip book test

--- index.php
+++ index.php
@@ -117,9 +117,12 @@
         </section> 
         <div>
             <object style="width:100%;height:100%;padding:0 !important;margin:0 !important;">
-                <param name="movie" value="http://static.issuu.com/webembed/viewers/style1/v2/IssuuReader.swf?mode=mini&amp;embedBackground=%23000000&amp;backgroundColor=%23222222&amp;documentId=120516040250-3b5bd3a7c8404b86b1fc36d0876f94b4" />
+                <param name="movie" value="http://static.issuu.com/webembed/viewers/style1/v2/IssuuReader.swf?mode=mini&amp;documentId=120516040250-3b5bd3a7c8404b86b1fc36d0876f94b4" />
                 <param name="allowfullscreen" value="true"/>
                 <param name="menu" value="false"/>
+                <param name="wmode" value="transparent"/>
+                <param name="scale" value="noborder" />
+                <param name="bgcolor" value="#ffffff" />
                 <param name="wmode" value="transparent"/>
                 <embed src="http://static.issuu.com/webembed/viewers/style1/v2/IssuuReader.swf" type="application/x-shockwave-flash" allowfullscreen="true" menu="false" wmode="transparent" />
             </object>
